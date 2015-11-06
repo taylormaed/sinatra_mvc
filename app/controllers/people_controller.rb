57a62b85d6c79c078e57f2e14925b8a6a2bd56a9
mtaylorelliott <tayls65@yahoo.com>
2015-11-05 20:35:50 -0600

    people show spec fails even though people page does function

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -41,7 +41,7 @@ end
 
 get '/people/:id' do
   @person = Person.find(params[:id])
-  birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
+  birth_path_num = Person.get_birthpath_num(@person.birthdate.strftime("%m%d%Y"))
   @message = Person.get_message(birth_path_num)  
   erb :"/people/show"
 end
